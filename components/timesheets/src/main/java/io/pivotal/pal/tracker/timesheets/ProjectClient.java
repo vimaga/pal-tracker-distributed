43ddd333a894b453040c9c83e238bed522d95b26
Vimagasivabalan <vimagasivabalan.marimuthu@cognizant.com>
2020-05-25 19:07:41 +0530

    Circuit Breaker Added

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,9 +1,16 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String endpoint;
 
@@ -13,6 +20,13 @@ public class ProjectClient {
     }
 
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo= restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectsCache.put(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return projectsCache.get(projectId);
     }
 }
